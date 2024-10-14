8007ad0328075bdda52dceb8591ff8b2b60f60f1
Steven Prybylynskyi <stepan.prybylynskyi@cloudbeds.com>
2024-10-14 13:08:33 +0200

    fix: update import statement for mkdirp

--- src/downloadTypes/helpers/downloadRemoteEntryTypes.ts
+++ src/downloadTypes/helpers/downloadRemoteEntryTypes.ts
@@ -2,7 +2,7 @@ import fs from 'node:fs';
 import path from 'node:path';
 
 import download from 'download';
-import mkdirp from 'mkdirp';
+import { mkdirp } from 'mkdirp';
 
 import { getLogger } from '../../helpers';
 
